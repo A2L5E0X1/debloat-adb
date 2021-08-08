9a460af4299e476ccd51d79f6c4b800a7606360f
root <joshua@razuuu.de>
2021-08-08 15:52:40 +0200

    debload-adb: Clear terminal before going to a script

--- script.sh
+++ script.sh
@@ -4,20 +4,26 @@
 # This Script is by A2L5E0X1
 #
 
+# Functions
+function run_script() {
+bash scripts/${@}.sh
+clear
+}
+
 clear
 echo "### Script Selector
 ### by A2L5E0X1"
 sleep 1
 
-echo -e "\nSelect a option"
+echo -e "\nSelect a option\n"
 
 while true; do
 		echo "(D)evice Debload | (O)ther Debload | (I)nstall Apps"
     read -p " » " select
     case $select in
-        [Dd]* ) bash scripts/device.sh; break;;
-				[Oo]* ) bash scripts/other.sh; break;;
-        [Ii]* ) bash scripts/install_apps.sh; break;;
+        [Dd]* ) run_script device; break;;
+	[Oo]* ) run_script other; break;;
+        [Ii]* ) run_script install_apps break;;
         [Ee]* ) echo "Exit script!"; exit 1;;
         * ) echo "No option selected!";;
     esac
