438dba993f6b73b040431ec8b7acd717c5622e84
R0rt1z2 <rogerortizleal@gmail.com>
2021-12-08 00:46:03 +0100

    debloat-adb: add BQ AOSP script
    * Only works on 5.0+ FW.
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

---
+++ type/BQ-AOSP.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# This Script is by A2L5E0X1 + Razuuu
+#
+
+# Get Variables and Functions
+. $(dirname "$(readlink -f "$0")")/../scripts/variables_and_functions.sh
+
+echo "### Example Debloat Script
+### by A2L5E0X1 + Razuuu
+### Thanks to R0rt1z2!"
+sleep 1
+
+bloatware=(
+	"com.estrongs.android.pop" # ES File Explorer
+	"com.bqreaders.ota" # Dead BQ OTAs App
+	"com.bqreaders.reader.nubicoapp" # Nubico
+	"com.bq.bqbookmark" # BQ Bookmarks
+	"com.bq.wizardbq.insurance" # BQ Protection Plus
+	"com.quickoffice.android" # Quick Office
+	"com.hp.android.printservice" # HP Print Service
+)
+
+for app in ${bloatware[@]}
+do
+	$adb_location shell pm uninstall --user 0 $app
+	$adb_location shell pm uninstall $app
+done
